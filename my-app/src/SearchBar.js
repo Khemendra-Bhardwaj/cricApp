298a9a0e127a3cc8c4de49a72901c9a2e5aae921
Khemendra-Bhardwaj <96744122+Khemendra-Bhardwaj@users.noreply.github.com>
2023-07-03 19:11:58 +0530

    SearchBox as seperate

--- my-app/src/SearchBar.js
+++ my-app/src/SearchBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ReactEcharts from 'echarts-for-react';
 import Selectbar from './Select';
 import Graph from './Graph';
+import SearchBox from './Search';
 // import find from './Select'
 
 export default function SearchBar() {
@@ -37,37 +38,37 @@ export default function SearchBar() {
     Country: '',
   });
 
-  const handleSearch = async () => {
-    const url = `https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/search?plrN=${playerName}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '31404b3dd1msh34e2a69d4cf0da2p1884c7jsn0020f6526ba2',
-        'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com',
-      },
-    };
+  // const handleSearch = async () => {
+  //   const url = `https://cricbuzz-cricket.p.rapidapi.com/stats/v1/player/search?plrN=${playerName}`;
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Key': '31404b3dd1msh34e2a69d4cf0da2p1884c7jsn0020f6526ba2',
+  //       'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com',
+  //     },
+  //   };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result);
-      await getThatPlayer(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const result = await response.json();
+  //     console.log(result);
+  //     await getThatPlayer(result);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  const getThatPlayer = async (result) => {
-    console.log('searching ...');
-    setPlayerInfo({
-      id: result.player[0].id,
-      Name: result.player[0].name,
-      Faceid: result.player[0].faceImageId,
-      Country: result.player[0].teamName,
-    });
-  };
+  // const getThatPlayer = async (result) => {
+  //   console.log('searching ...');
+  //   setPlayerInfo({
+  //     id: result.player[0].id,
+  //     Name: result.player[0].name,
+  //     Faceid: result.player[0].faceImageId,
+  //     Country: result.player[0].teamName,
+  //   });
+  // };
 
-  const battingStats = async (result) => {  //populating stats 2d array Dont make 2
+  const carrerStats = async (result) => {  //populating stats 2d array Dont make 2
     const stat = [[]];
     const l = ['Field', 'Test', 'Odi', 'T20', 'Ipl'];
     stat.push(l);
@@ -100,7 +101,7 @@ export default function SearchBar() {
       const response = await fetch(url, options);
       const battingResult = await response.json();
       console.log(battingResult);
-      battingStats(battingResult);
+      carrerStats(battingResult);
     } catch (error) {
       console.error(error);
     }
@@ -115,7 +116,7 @@ export default function SearchBar() {
 
   return (
     <>
-      Search:{' '}
+      {/* Search:{' '}
       <input
         type="text"
         placeholder="enter Player name"
@@ -123,7 +124,8 @@ export default function SearchBar() {
       />
       <button type="submit" onClick={handleSearch}>
         Search
-      </button>
+      </button> */}
+      <SearchBox playerName={playerName} setPlayerInfo={setPlayerInfo} setPlayerName={setPlayerName} />
 
       <h1>{playerInfo.Name}</h1>
       {/* <h1>{playerInfo.id}</h1> */}
